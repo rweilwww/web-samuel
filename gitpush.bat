394623b93dd411b2d46a70dc499aa90bd1b87cc3
rweilwww <github@shaw-weil.com>
2025-11-08 18:11:22 -0500

    from inDesign 2025-11-08 23:09

--- gitpush.bat
+++ gitpush.bat
@@ -1,14 +1,9 @@
 
 cd /inetpub/wwwroot/w/samuel
-git init
-            git config --global --add safe.directory C:/inetpub/wwwroot/w/samuel
-   git remote add origin https://github.com/rweilwww/web-samuel.git
-   git remote -v
 
-        
 
 git add .
-git commit -m "from inDesign 2025-11-08 22:51" --no-edit
+git commit -m "from inDesign 2025-11-08 23:09" --no-edit
 git push --force --set-upstream origin main
 
 rem wait for one minute to allow git hub to get its act together
